1533701bbc34d309d0f4d0ee139c2c5c694801bb
Tinashe Ndudzo <nashie54@gmail.com>
2023-07-18 13:50:36 -0700

    Task 3

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+*print_alphabet_x10 - Make the alphabet 10 times
+*
+*Return:void
+*/
+
+void print_alphabet_x10(void)
+{
+	char ch;
+	int j;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+
+	_putchar('\n');
+
+	}
+}
