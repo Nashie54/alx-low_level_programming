1a37ea4d32fe065804e0ea3201fb3384d45ab94c
Tinashe Ndudzo <nashie54@gmail.com>
2023-07-20 11:50:08 -0700

    Task 11:101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,41 +8,31 @@
  */
 void print_number(int n)
 {
-	int m; /* power of 10 */
-	int c; /* boolean check */
-	int num; /* convert int to long */
+    int power_of_10 = 1;
+    int num = n;
 
-	num = n;
-	/* Handle negatives */
-	if (num < 0)
-	{
-		num *= -1;
-		_putchar('-');
-	}
+    /* Handle negatives */
+    if (num < 0)
+    {
+        num *= -1;
+        _putchar('-');
+    }
 
-	/* Count up */
-	m = 1;
-	c = 1;
-	while (c)
-	{
-		if (num / (m * 10) > 0)
-			m *= 10;
-		else
-			c = 0;
-	}
+    /* Calculate the power of 10 */
+    while (num / power_of_10 >= 10)
+    {
+        power_of_10 *= 10;
+    }
 
-	/* Count down */
-	while (num >= 0)
-	{
-		if (m == 1)
-		{
-			_putchar(num % 10 + '0');
-			num = -1;
-		}
-		else
-		{
-			_putchar((num / m % 10) + '0');
-			m /= 10;
-		}
-	}
+    /* Print each digit */
+    while (power_of_10 > 0)
+    {
+        _putchar((num / power_of_10) + '0');
+        num %= power_of_10;
+        power_of_10 /= 10;
+    }
+
+    /* If the number was 0, print 0 */
+    if (n == 0)
+        _putchar('0');
 }
