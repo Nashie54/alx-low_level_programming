c541d0faccfa7b831769c26d4b1f27b42124b266
Tinashe Ndudzo <nashie54@gmail.com>
2023-07-20 13:08:07 -0700

    Task 11

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,16 +7,19 @@
  */
 void print_number(int n)
 {
+	unsigned int k = n;
+
 	if (n < 0)
 	{
+		n *= -1;
+		k = n;
 		_putchar('-');
-		n = -n;
 	}
 
-	if (n / 10 != 0)
-	{
-		print_number(n / 10);
-	}
+	k /= 10;
+	if (k != 0)
+		print_number(k);
 
-	_putchar(n % 10 + '0');
+	_putchar((unsigned int) n % 10 + '0');
+
 }
