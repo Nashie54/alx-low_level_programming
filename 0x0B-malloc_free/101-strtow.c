a23bbd5b2756f6078d7d51fac24d52da35c875a4
Tinashe Ndudzo <nashie54@gmail.com>
2023-08-23 03:00:49 -0700

    Task 6

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -2,6 +2,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * number - function to calculate number of words
+ * @str: string being passed to check for words
+ *
+ * Return: number of words
+ */
 int number(char *str)
 {
 	int a, num = 0;
@@ -20,6 +26,11 @@ int number(char *str)
 	return (num);
 }
 
+/**
+ * free_everything - frees the memory
+ * @string: pointer values being passed for freeing
+ * @i: counter
+ */
 void free_everything(char **string, int i)
 {
 	for (; i > 0;)
@@ -27,6 +38,11 @@ void free_everything(char **string, int i)
 	free(string);
 }
 
+/**
+ * strtow - function that splits string into words
+ * @str: string being passed
+ * Return: null if string is empty or null or function fails
+ */
 char **strtow(char *str)
 {
 	int total_words = 0, b = 0, c = 0, length = 0;
@@ -34,15 +50,12 @@ char **strtow(char *str)
 
 	if (str == 0 || *str == 0)
 		return (NULL);
-
 	total_words = number(str);
 	if (total_words == 0)
 		return (NULL);
-
 	words = malloc((total_words + 1) * sizeof(char *));
 	if (words == 0)
 		return (NULL);
-
 	for (; *str != '\0' && b < total_words;)
 	{
 		if (*str == ' ')
