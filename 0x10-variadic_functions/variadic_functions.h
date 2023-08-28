d15590985f7b80fed823894cc92bcf74057d7aff
Tinashe Ndudzo <nashie54@gmail.com>
2023-08-28 02:11:13 -0700

    Task 0 Corrections

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,13 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 #include <stdarg.h>
+
+/**
+ * struct what_format - defines representing a data type
+ * @type: the data type
+ * @f: function that prints data type in correct fmt
+ */
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
