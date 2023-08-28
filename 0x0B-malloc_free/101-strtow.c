074e3920260599ec43951372ef29440ae643fbf3
Tinashe Ndudzo <nashie54@gmail.com>
2023-08-28 01:07:46 -0700

    Task 6 Corrections

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -25,7 +25,6 @@ int number(char *str)
 	}
 	return (num);
 }
-
 /**
  * free_everything - frees the memory
  * @string: pointer values being passed for freeing
@@ -56,7 +55,7 @@ char **strtow(char *str)
 	words = malloc((total_words + 1) * sizeof(char *));
 	if (words == 0)
 		return (NULL);
-	for (; *str != '\0' && b < total_words;)
+	for (; *str != '\0' &&  b < total_words;)
 	{
 		if (*str == ' ')
 			str++;
@@ -81,12 +80,8 @@ char **strtow(char *str)
 				c++;
 			}
 			words[b][c] = '\0';
-			b++;
-			c = 0;
-			length = 0;
-			str++;
+			b++; c = 0; length = 0; str++;
 		}
 	}
 	return (words);
 }
-
